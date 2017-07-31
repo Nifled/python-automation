af2593e794cb2a1f2d98df4fdb78cbe15b700c2f
Erick Delfin <erickdelfinmolina@gmail.com>
2017-07-31 00:43:55 -0700

    Add individual functions for tasks in pimple

--- pimple/pimple.py
+++ pimple/pimple.py
@@ -1,13 +1,62 @@
 import os
 from PIL import Image
 
-os.makedirs('popped', exist_ok=True)
+# \
+#  Pre-defined constants
+# /
+IMAGE_QUALITY = 75
+RESIZE_OPTION = True
+RESIZE_PERCENT = 50
+RES_THRESHOLD = (1080, 720)
 
-print('Optimizing images...')
+# TEMP
+DIRNAME = '.'
 
-for file_name in os.listdir('.'):
-    if not (file_name.endswith('.png') or file_name.endswith('.jpg')):
-        continue
+def checks(w, h):
+    """w = Width, h = Height"""
+    if w < RES_THRESHOLD[0] and h < RES_THRESHOLD[1]:
+        return True
+    return False
 
-    img = Image.open(file_name)
-    img.save(os.path.join('popped', file_name), optimize=True)
+def resize_img(_img):
+    w, h = _img.size
+    new_w, new_h = w * RESIZE_PERCENT / 100, h * RESIZE_PERCENT / 100
+
+    _img.thumbnail((new_w, new_h), Image.ANTIALIAS)
+    return
+
+
+def compress_img(file_name, resize=False):
+    print(file_name)
+    ext = file_name.rsplit('.')[-1]
+    if not (ext == 'png' or ext == 'jpg'):
+        return
+
+    image = Image.open(file_name)
+
+    if resize:
+        resize_img(image)
+
+    image.save(
+        os.path.join('popped', file_name),
+        optimize=True,
+        quality=IMAGE_QUALITY
+    )
+
+def compress_dir(dirname):
+    for file_name in os.listdir(dirname):
+        compress_img(file_name, resize=RESIZE_OPTION)
+    return
+
+
+def main():
+    os.makedirs('popped', exist_ok=True)
+
+    print('Optimizing images...')
+
+    compress_dir(DIRNAME)
+
+    print('Done!')
+
+if __name__ == '__main__':
+    main()
