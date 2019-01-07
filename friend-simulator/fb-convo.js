1197869335390fe4f19afd119bf63ad8be15d759
nifled <erickdelfinmolina@gmail.com>
2019-01-06 23:55:22 -0700

    Add a group by day function

--- friend-simulator/fb-convo.js
+++ friend-simulator/fb-convo.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const rawdata = fs.readFileSync("dania.json");
+const rawdata = fs.readFileSync("manny.json");
 const data = JSON.parse(rawdata);
 
 const messages = data.messages;
@@ -9,8 +9,8 @@ const separated = groupBy(messages, "sender_name");
 const a = separated[Object.keys(separated)[0]];
 const b = separated[Object.keys(separated)[1]];
 
-console.log(a.slice(0, 1));
-console.log(getTotalWords(a, "content"));
+// console.log(a.slice(0, 5));
+console.log(groupByDay(a.slice(0, 50), "timestamp_ms"));
 
 // {
 //   "sender_name": "Erick",
@@ -22,13 +22,28 @@ console.log(getTotalWords(a, "content"));
 // Inspiration - https://i.redd.it/23v9czqdj6i01.jpg
 
 // utils
-function groupBy(objectArray, property) {
-  return objectArray.reduce((acc, obj) => {
+function groupBy(array, property) {
+  return array.reduce((acc, obj) => {
     let key = obj[property];
     if (!acc[key]) {
       acc[key] = [];
     }
     acc[key].push(obj);
+    return acc;
+  }, {});
+}
+
+function groupByDay(array, property) {
+  return array.reduce((acc, msg) => {
+    let date = new Date(msg[property]);
+    date.setHours(0, 0, 0); // Without hr, min, ms
+    // let msDate = date.getTime();
+    let str = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate();
+    // console.log(str);
+    if (!acc[str]) {
+      acc[str] = [];
+    }
+    acc[str].push(msg);
     return acc;
   }, {});
 }
